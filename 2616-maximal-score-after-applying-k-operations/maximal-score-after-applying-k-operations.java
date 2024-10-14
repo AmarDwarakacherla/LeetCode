a6141795eb5135b39a308c57c0553c22864952a8
Amar Dwarakacherala <73354017+Ameilla@users.noreply.github.com>
2024-10-14 17:11:30 +0530

    Time: 152 ms (58.77%) | Memory: 63.3 MB (20.38%) - LeetSync

---
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long maxKelements(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int num : nums) {
+            maxHeap.add(num);
+        }
+        long score = 0;
+        for (int i = 0; i < k; i++) {
+            int maxVal = maxHeap.poll();
+            score += maxVal;
+            int newVal = (int) Math.ceil(maxVal / 3.0);
+            maxHeap.add(newVal);
+        }
+        return score;
+    }
+}
