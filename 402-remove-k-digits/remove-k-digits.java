aeb9ae761dee93b4de56d47a3f99a36212aa5a66
Amar Dwarakacherla <73354017+AmarDwarakacherla@users.noreply.github.com>
2025-09-01 19:30:52 +0530

    Time: 21 ms (59.14%) | Memory: 45 MB (79.52%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,31 +1,27 @@
 class Solution {
     public String removeKdigits(String num, int k) {
-        Stack<Character> stack = new Stack<>();
-        for(char ch: num.toCharArray())
-        {
-            while(!stack.isEmpty() && stack.peek()>ch && k>0)
-            {
-                stack.pop();
-                k--;
-            }
-            stack.push(ch);
+         Stack<Character> stack = new Stack<>();
+        //check weather the peak element is big or not
+        for(char ch : num.toCharArray()){
+           while(!stack.isEmpty() && k>0 && stack.peek()>ch){
+               stack.pop();
+               k--;
+           }
+           stack.push(ch);
         }
-        while(!stack.isEmpty() && k>0)
-        {
+        //If not found any element the remove the last element from the stack until k
+        while(!stack.isEmpty() && k>0){
             stack.pop();
             k--;
         }
         StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty())
-        {
-            sb.append(stack.pop());
+        for(char ch: stack){
+            sb.append(ch);
         }
-        sb.reverse();
-        // if(sb.length()>1 && sb.charAt(0)=='0')
-        //         sb.deleteCharAt(0);
-        while(sb.length()>1 && sb.charAt(0)=='0')
+        //Removing the leading zeros
+        while(sb.length()>0 && sb.charAt(0) == '0'){
             sb.deleteCharAt(0);
-        return sb.length()>0 ? sb.toString() : "0";
-        
+        }
+        return sb.length()==0 ? "0" : sb.toString();
     }
 }
