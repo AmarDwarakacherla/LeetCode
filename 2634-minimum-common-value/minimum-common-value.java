b1f2b9635cd875b354e9afb585e734efdf9e4d02
Amar Dwarakacherla <73354017+AmarDwarakacherla@users.noreply.github.com>
2026-05-19 06:46:15 +0530

    Time: 16 ms (12.85%) | Memory: 93.9 MB (5.73%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,15 +1,15 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-       int i=0,j=0;
-        while(i<nums1.length && j<nums2.length)
-        {
-            if(nums1[i]==nums2[j])
-                return nums1[i];
-            else if(nums1[i]<nums2[j])
-                i++;
-            else
-                j++;
+       Set<Integer> seen = new HashSet<>();
+        for(int num : nums1){
+            seen.add(num);
         }
-        return -1;  
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int num : nums2){
+            if(seen.contains(num)){
+                list.add(num);
+            }
+        }
+        return list.isEmpty() ? -1 : list.getFirst();
     }
 }
